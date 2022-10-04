417a720f71c52976af47923a4c6af04907578093
Fellipe A. de azevedo <fellipe.azevedo@kian.com.br>
2022-10-04 09:31:07 -0300

    Updates

---
+++ precache-manifest.cb3c2c42219cacd3bdb9292e48ee478c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32560e1d1391282d0e6",
+    "url": "/static/css/main.d0669cde.chunk.css"
+  },
+  {
+    "revision": "c32560e1d1391282d0e6",
+    "url": "/static/js/main.c32560e1.chunk.js"
+  },
+  {
+    "revision": "a2b3517d29fb5dd4a79f",
+    "url": "/static/js/1.a2b3517d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a1ba15c3e4021f0541fa3c1b92a72b4",
+    "url": "/index.html"
+  }
+];
