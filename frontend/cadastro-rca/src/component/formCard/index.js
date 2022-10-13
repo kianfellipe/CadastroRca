4ba5898ea36ba33d33a954c14966adcaa2b2794e
Fellipe A. de azevedo <fellipe.azevedo@kian.com.br>
2022-10-13 10:08:00 -0300

    Código reformulado para que o método POST enviasse formulários com anexo para o banco de dados

--- frontend/cadastro-rca/src/component/formCard/index.js
+++ frontend/cadastro-rca/src/component/formCard/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import ReactInputMask from 'react-input-mask'
 import './styles.css'
-import axios from 'axios'
 import icon from './add.png'
+import axios from 'axios'
 
 
 const stGen = {
@@ -30,9 +30,11 @@ const stEnd4 = {
 
 
 
-function Rca() {
-     
-    const [rca, setRca] = useState ({
+class Rca extends Component {
+
+
+    state = {
+        file: null,
         fullName:'',
         email:'',
         tel:'',
@@ -46,61 +48,87 @@ function Rca() {
         bairro:'',
         cidade:'',
         estado:'',
-        anexId:'',
-        anexCpf: '',
-        anexResid:''
-    
-    })
-    
-    const {fullName, email, tel, cpf, cnpj, sex, bDate, cep, endereco, num, bairro, cidade, estado, anexId, anexCpf, anexResid} = rca
-    
-    const onInputChange = (e) => {
-        setRca({ ...rca, [e.target.name]: e.target.value })
-    }
-    
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        await axios.post('http://localhost:8080/nova', rca)
-    
     }
 
-    return (
- 
+
+
+    handleFile(e) {
+        let file = e.target.files[0]
+        this.setState({ file: file })
         
-                <div className="box">
-                    <form onSubmit={(e) => onSubmit(e)}>
-                        <fieldset>
-                            <legend className="title">Cadastro RCA</legend>
+        
+    }
 
 
-                            <div className="inputBox">
-                                <input type={"text"} 
+    onSubmit(e) {
+        console.log(this.state, 'THE STATE --------- $$$$')
+
+        let file = this.state.file
+        let rca = new Blob([JSON.stringify({
+            fullName: this.state.fullName,
+            email: this.state.email,
+            tel: this.state.tel,
+            cpf: this.state.cpf,
+            cnpj: this.state.cnpj,
+            sex: this.state.sex,
+            bDate: this.state.bDate,
+            cep: this.state.cep,
+            num: this.state.num,
+            bairro: this.state.bairro,
+            endereco: this.state.endereco,
+            cidade: this.state.cidade,
+            estado: this.state.estado
+          })], {
+          type: 'application/json'
+        })
+
+        let formData = new FormData()
+
+        formData.append('file', file)
+        formData.append('rca', rca)
+
+        axios({
+            url: "http://localhost:8080/nova",
+            method: "POST",
+            data: formData
+        })
+
+    }
+
+    render() {
+        return (
+
+            <div className="box">
+
+                <fieldset>
+                    <legend className="title">Cadastro RCA</legend>
+                    <div className="inputBox">
+                                <input type="text"
                                 name="fullName" 
                                 className="inputUser" 
                                 autoComplete="off"
-                                value={fullName}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.fullName} 
+                                onChange={e => this.setState({fullName: e.target.value})}/>
                                 <label htmlFor="nome" className="labelInput">Nome Completo</label>
                             </div>
-
                             <div className="inputBox">
                                 <input type="text" 
                                 name="email" 
                                 className="inputUser" 
                                 autoComplete="off"
-                                value={email}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.email}
+                                onChange={e => this.setState({email: e.target.value})}/>
                                 <label htmlFor="email" className="labelInput">E-mail</label>
                             </div>
 
                             <div className="inputBox">
                                 <ReactInputMask mask="(99)99999-9999" 
-                                type="tel" name="tel" 
+                                type="text" name="tel" 
                                 id="telefone" 
                                 className="inputUser" 
                                 autoComplete="off"
-                                value={tel}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.tel}
+                                onChange={e => this.setState({tel: e.target.value})}/>
                                 <label htmlFor="telefone" className="labelInput">Telefone</label>
                             </div>
 
@@ -109,22 +137,22 @@ function Rca() {
                                 type="text" name="cpf" className="inputUser" 
                                 id="input_cpf" 
                                 autoComplete="off"
-                                value={cpf}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.cpf}
+                                onChange={e => this.setState({cpf: e.target.value})}/>
                                 <label htmlFor="cpf" className="labelInput">CPF</label>
                             </div>
 
                             <div className="inputBox">
                                 <ReactInputMask mask="99.999.999/9999-99" type="text" name="cnpj" 
                                 id="cnpj" className="inputUser"
-                                value={cnpj}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.cnpj}
+                                onChange={e => this.setState({cnpj: e.target.value})}/>
                                 <label htmlFor="cnpj" className="labelInput">CNPJ</label>
                             </div>
                             <div className="inputBox" style={stGen}>
                                 Sexo:
-                                <div className="radio"  value={sex}
-                                onChange={(e) => onInputChange(e)}>
+                                <div className="radio"  value={this.state.sex}
+                                onChange={e => this.setState({sex: e.target.value})}>
                                     <div className="input_div">
                                         <label htmlFor="feminino">Feminino</label>
                                         <input type="radio" id="feminino" name="genero" value="feminino"  
@@ -149,8 +177,8 @@ function Rca() {
                                 <input type="date" 
                                 name="bDate" 
                                 id="data_nascimento"  
-                                value={bDate}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.bDate}
+                                onChange={e => this.setState({bDate: e.target.value})}/>
                             </div>
 
 
@@ -158,8 +186,8 @@ function Rca() {
                             <div className="inputBox">
                                 <ReactInputMask mask="99999-999" type="text" name="cep" 
                                 id="cep" className="inputUser" 
-                                value={cep}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.cep}
+                                onChange={e => this.setState({cep: e.target.value})}/>
                                 <label htmlFor="cep" className="labelInput">CEP</label>
                             </div>
 
@@ -167,16 +195,16 @@ function Rca() {
                                 <div style={stEnd2}>
                                     <input type="text" name="endereco" id="endereco" className="inputUser" 
                                     autoComplete="off"
-                                    value={endereco}
-                                    onChange={(e) => onInputChange(e)} />
+                                    value={this.state.endereco}
+                                    onChange={e => this.setState({endereco: e.target.value})} />
                                     <label htmlFor="endereco" className="labelInput">Endereço</label>
                                 </div>
 
                                 <div style={stEnd3}>
                                     <input type="number" name="num" id="numero" className="inputUser" 
                                     autoComplete="off"
-                                    value={num}
-                                    onChange={(e) => onInputChange(e)}/>
+                                    value={this.state.num}
+                                    onChange={e => this.setState({num: e.target.value})}/>
                                     <label htmlFor="numero" className="labelInput" style={stEnd4}>N°</label>
                                 </div>
                             </div>
@@ -184,80 +212,58 @@ function Rca() {
                             <div className="inputBox">
                                 <input type="text" name="bairro" id="bairro" className="inputUser"  
                                 autoComplete="off"
-                                value={bairro}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.bairro}
+                                onChange={e => this.setState({bairro: e.target.value})}/>
                                 <label htmlFor="bairro" className="labelInput">Bairro</label>
                             </div>
 
                             <div className="inputBox">
                                 <input type="text" name="cidade" id="cidade" className="inputUser"  
                                 autoComplete="off"
-                                value={cidade}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.cidade}
+                                onChange={e => this.setState({cidade: e.target.value})}/>
                                 <label htmlFor="cidade" className="labelInput">Cidade</label>
                             </div>
 
                             <div className="inputBox">
                                 <input type="text" name="estado" id="estado" className="inputUser"  
                                 autoComplete="off"
-                                value={estado}
-                                onChange={(e) => onInputChange(e)}/>
+                                value={this.state.estado}
+                                onChange={e => this.setState({estado: e.target.value})}/>
                                 <label htmlFor="estado" className="labelInput">Estado</label>
                             </div>
 
 
 
-                            <div className="arquivos">
-                                <div className="inputBox">
-                                    <span>Identidade (Anexo):</span>
-                                    <div className="inputFile">
-                                        <label className="labelArquivos" htmlFor="identidade" id="identidade_L">
-                                            <img className="imageInput" src={icon} alt="ident" />
+                    <div className="arquivos">
+                        <div className="inputBox">
+                            <span>Identidade (Anexo):*</span>
+                            <div className="inputFile">
+                              
 
-                                        </label>
-                                        <input type="file" name="anexId" id="identidade"  
-                                        value={anexId}
-                                        onChange={(e) => onInputChange(e)}/>
-
-                                    </div>
+                                <div>
+                                    <input type='text' value={this.state.email} 
+                                     onChange={e => this.setState({email: e.target.value})}></input>
                                 </div>
+                                <label className="labelArquivos" htmlFor="identidade" id="identidade_L">
+                                    <img className="imageInput" src={icon} alt="ident" />
 
-                                <div className="inputBox">
-                                    <span>CPF (Anexo):</span>
+                                </label>
+                                <input type="file" id="identidade"
 
-                                    <div className="inputFile">
-                                        <label className="labelArquivos" htmlFor="CPF" id="CPF_L">
-                                            <img className="imageInput" id="CPF_I" src={icon} alt="cpf" />
+                                    onChange={(e) => this.handleFile(e)} />
 
-                                        </label>
-                                        <input type="file" name="anexCpf" id="CPF"  
-                                        value={anexCpf}
-                                        onChange={(e) => onInputChange(e)}/>
-                                    </div>
-
-                                </div>
-
-                                <div className="inputBox">
-                                    <span>Comprovante de residência (Anexo):</span>
-                                    <div className="inputFile">
-                                        <label className="labelArquivos" id="residencia_L" htmlFor="residencia">
-                                            <img className="imageInput" id="residencia_I" src={icon} alt="compr" />
-                                        </label>
-                                        <input type="file" name="anexResid" id="residencia"  
-                                        value={anexResid}
-                                        onChange={(e) => onInputChange(e)}/>
-                                    </div>
-                                </div>
                             </div>
+                        </div>
+                    </div>
+                    <input type="submit" onClick={(e) => this.onSubmit(e)} />
+                </fieldset>
 
-                            <input type="submit" name="submit" id="submit" />
-                           
-                        </fieldset>
-                    </form>
-                </div>
+            </div>
 
-         
-    );
+
+        );
+    }
 }
 
 export default Rca;
